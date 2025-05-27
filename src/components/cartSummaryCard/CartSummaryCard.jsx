9a15bd589beb2f69a9878b4a359cf54894b50724
AdaskoKwi <adamnowakhomm@gmail.com>
2025-05-27 22:30:16 +0200

    Added functionality to buy button on cart page.

--- src/components/cartSummaryCard/CartSummaryCard.jsx
+++ src/components/cartSummaryCard/CartSummaryCard.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import './CartSummaryCard.css'
+import {Link} from "react-router-dom";
 
 const CartSummaryCard = ({cartItems, cartSum, setCartSum}) => {
+    const BASE_PRODUCT_API_URL = 'http://localhost:8080/api/v1/products'
+
+    const removeCartItemsFromStock = async () => {
+        try {
+            console.log('Sending payload:', JSON.stringify(cartItems));
+
+            const response = await fetch(BASE_PRODUCT_API_URL + '/update/collection', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cartItems)
+            });
+
+            if (!response.ok) {
+                console.error("Couldn't remove cart items from stock");
+            }
+
+            alert("Bought items!")
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     const calculateCartPrice = () => {
         let sum = 0;
@@ -22,7 +46,9 @@ const CartSummaryCard = ({cartItems, cartSum, setCartSum}) => {
                 <p className={"price"}>{cartSum.toFixed(2)} zł</p>
             </div>
 
-            <button>Kup ▸</button>
+            <Link to={'/'}>
+                <button onClick={removeCartItemsFromStock}>Kup ▸</button>
+            </Link>
         </div>
     )
 }
